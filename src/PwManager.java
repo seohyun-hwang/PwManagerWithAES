0cafe8b3fb56b398c9eb00693d175077c475128d
seohyun-hwang <seohyun.hwang07@gmail.com>
2025-08-18 18:41:00 -0700

    Version 1.0

---
+++ src/PwManager.java
@@ -0,0 +1,157 @@
+import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.*;
+import java.io.*;
+
+public class PwManager {
+    private String masterPw = ""; //the password required to access the password manager
+    private List<PwInfo> pwInfoList = new ArrayList<PwInfo>();
+    private static String dataFileName = "pwManager.data";
+
+    //master password check against password attempt
+    public Boolean checkMasterPw(String pwInput) {
+        return pwInput.equals(masterPw);
+    }
+
+    //change master password
+    public void changeMasterPw(String masterPw) {
+        this.masterPw = masterPw;
+    }
+    //get password info list
+    public List<PwInfo> getPwInfoList() {
+        return pwInfoList;
+    }
+
+
+    //generates the initialization vector (iv)
+    public static GCMParameterSpec generateIv() {
+        byte[] iv = new byte[12];
+        new SecureRandom().nextBytes(iv);
+        return new GCMParameterSpec(128, iv);
+    }
+    //generation of AES secret-key for encryption/decryption via PBKDF2 with 2^8 iterations
+    public static SecretKey getKeyFromPassword(String masterPw) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(masterPw.toCharArray(), "salt".getBytes(), 256, 256);
+        SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+        return secret;
+    }
+    //encrypts the data of the given object
+    private static SealedObject encryptObject(Serializable object, String masterPw) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+            InvalidKeyException, IOException, IllegalBlockSizeException, InvalidKeySpecException {
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, getKeyFromPassword(masterPw), generateIv());
+        SealedObject sealedObject = new SealedObject(object, cipher);
+        return sealedObject;
+    }
+    //decrypts the data of the given object
+    private static Serializable decryptObject(SealedObject sealedObject, String masterPw) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidKeyException, ClassNotFoundException, BadPaddingException, IllegalBlockSizeException,
+            IOException, InvalidKeySpecException, InvalidAlgorithmParameterException {
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, getKeyFromPassword(masterPw), generateIv());
+        Serializable unsealObject = (Serializable) sealedObject.getObject(cipher);
+        return unsealObject;
+    }
+    public boolean encryptAndSave() {
+        try {
+            FileOutputStream file = new FileOutputStream(dataFileName);
+            file.getChannel().truncate(0);
+            ObjectOutputStream out = new ObjectOutputStream(file);
+
+            out.writeObject(encryptObject(masterPw, masterPw)); //writes encrypted master-pw
+            out.writeObject(encryptObject((ArrayList<PwInfo>) pwInfoList, masterPw)); //writes encrypted pwInfoList array
+
+            out.close();
+            file.close();
+
+            return true;
+        } catch(IOException ex) {
+            return false;
+        } catch (InvalidAlgorithmParameterException e) {
+            return false;
+        } catch (NoSuchPaddingException e) {
+            return false;
+        } catch (IllegalBlockSizeException e) {
+            return false;
+        } catch (NoSuchAlgorithmException e) {
+            return false;
+        } catch (InvalidKeySpecException e) {
+            return false;
+        } catch (InvalidKeyException e) {
+            return false;
+        }
+    }
+    public boolean loadAndDecrypt(String masterPw) {
+        try {
+            if (!new File(dataFileName).exists()) {
+                return true;
+            }
+
+            FileInputStream file = new FileInputStream(dataFileName);
+            ObjectInputStream in = new ObjectInputStream(file);
+
+            this.masterPw = (String) decryptObject((SealedObject) in.readObject(), masterPw);
+            this.pwInfoList = (List<PwInfo>) decryptObject((SealedObject) in.readObject(), masterPw);
+
+            in.close();
+            file.close();
+
+            return true;
+        } catch(IOException ex) {
+            return false;
+        } catch(ClassNotFoundException ex) {
+            return false;
+        } catch (NoSuchPaddingException e) {
+            return false;
+        } catch (IllegalBlockSizeException e) {
+            return false;
+        } catch (NoSuchAlgorithmException e) {
+            return false;
+        } catch (BadPaddingException e) {
+            return false;
+        } catch (InvalidKeySpecException e) {
+            return false;
+        } catch (InvalidKeyException e) {
+            return false;
+        } catch (InvalidAlgorithmParameterException e) {
+            return false;
+        }
+    }
+
+    //look-up a requested title
+    public PwInfo lookUp(String title) {
+        for (PwInfo info: pwInfoList) { //traverse through the ArrayList
+            if (info.getTitle().equals(title)) { //check whether requested title matches the index
+                return info;
+            }
+        }
+        return null;
+    }
+    //add an entry
+    public boolean addPwInfo(String title, String username, String password, String siteURL) {
+        if (lookUp(title) == null) {
+            pwInfoList.add(new PwInfo(title, username, password, siteURL));
+            return true;
+        }
+        return false;
+    }
+    //delete an entry
+    public boolean deletePwInfo(String title) {
+        PwInfo info = lookUp(title);
+        if (info != null) {
+            pwInfoList.remove(info);
+            return true;
+        }
+        return false;
+    }
+}
